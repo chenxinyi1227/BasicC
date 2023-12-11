ce97d5b693c982a0b2b75cb699780bd3a42f2943
cxy <2830206832@qq.com>
2023-12-11 16:43:21 +0800

    [新增]gcc编译

---
+++ demoMakefile/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int printfString()
+{
+    int ret = 0;
+    printf("print string!\n");
+    return ret;
+}
+int main()
+{
+    int num1 = 10;
+    printf("num1= %d\n",num1);
+    printfString();
+    return 0;
+}
