c98489b2f3a670a989b7f8efc88d52694a16f33b
cxy <2830206832@qq.com>
2023-12-27 20:54:45 +0800

    [修改]位置

---
+++ demoMakefile/demo1/main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// int printfString()
+// {
+//     int ret = 0;
+//     printf("print string!\n");
+//     return ret;
+// }
+
+// int selectChoice(int choice)
+// {
+//     switch (choice)
+//     {
+//     case 1:
+//         printf("print 1\n");
+//         break;
+//     case 2:
+//         printf("print 2\n");
+//     break;
+//     default:
+//         break;
+//     }
+// }
+int main()
+{
+    printf("hello world\n");
+    return 0;
+}
